12706eb665ea05f15da35e7b52935a8af9ac0249
camillaryskjaer <camr@zbc.dk>
2020-08-07 08:52:12 +0200

    Update Program.cs
    Husk ikke at blande UI og Logik laget!

--- Drinks/Program.cs
+++ Drinks/Program.cs
@@ -10,11 +10,14 @@ namespace Drinks
     {
         static void Main(string[] args)
         {
+            
+            //UPS du blander både logik og UI i samme klasse! Alle alarmklokker burde ringe ...
             List<Ingredient> ingredients = new List<Ingredient>();
             ingredients.Add(new Ingredient() { Name = "Vodka" });
             ingredients.Add(new Ingredient() { Name = "Redbull" });
             using (var ctx = new RepositoryContext())
             {
+                //Fint med et repository pattern, det giver ++
                 DrinkRepository DR = new DrinkRepository(ctx);
                 List<Drink> loopDrinks = DR.FindDrinksByIngredient("Vodka");
                 foreach (var item in loopDrinks)
